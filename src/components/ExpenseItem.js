e782615d940e51e53b1a4d304d6167fb623d92c6
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-05 22:10:05 +0530

    removed comments

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -16,20 +16,3 @@ function ExpenseItem() {
 }
 
 export default ExpenseItem;
-
-
-// function ExpenseItem() {
-//   const expenseDate = new Date(2021, 2, 28);
-//   const expenseTitle = 'Car Insurance';
-//   const expenseAmount = 294.67;
-
-//   return (
-//     <div className='expense-item'>
-//       <div>{expenseDate.toISOString()}</div>
-//       <div className='expense-item__description'>
-//         <h2>{expenseTitle}</h2>
-//         <div className='expense-item__price'>${expenseAmount}</div>
-//       </div>
-//     </div>
-//   );
-// }
