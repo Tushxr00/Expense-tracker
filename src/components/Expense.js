271572b5b29a4c7f39a7249c4f0603831c98f849
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-06 21:54:13 +0530

    applying card component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,8 +1,11 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expense.css";
+
+
 function Expense(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
@@ -23,7 +26,7 @@ function Expense(props) {
         amount={props.expenses[3].amount}
         date={props.expenses[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
