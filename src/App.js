03c4b8c13c0bf5d8707a94cbadca114ced677702
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-06 20:12:04 +0530

    adding props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expesnes = [
+    {
+      id: `e1`,
+      title: `Car Insurance`,
+      amount: 296.74,
+      date: new Date(2022, 4, 5),
+    },
+    {
+      id: `e2`,
+      title: `Car Insurance`,
+      amount: 296.74,
+      date: new Date(2022, 4, 5),
+    },
+    {
+      id: `e3`,
+      title: `Car Insurance`,
+      amount: 296.74,
+      date: new Date(2022, 4, 5),
+    },
+    {
+      id: `e4`,
+      title: `Car Insurance`,
+      amount: 296.74,
+      date: new Date(2022, 4, 5),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,7 +9,7 @@ function ExpenseItem() {
       <div> {expenseDate.toISOString()} </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <div className="expense-item__price">$ {expenseAmount}</div>
       </div>
     </div>
   );
