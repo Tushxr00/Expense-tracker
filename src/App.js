4c29bb4dbc98ebdc64829a8a329ddcae0a746517
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-06 20:23:39 +0530

    applied props on react commponents

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
-  const expesnes = [
+  const expenses = [
     {
       id: `e1`,
-      title: `Car Insurance`,
-      amount: 296.74,
-      date: new Date(2022, 4, 5),
+      title: `Crypto`,
+      amount: 3000,
+      date: new Date(2022, 3, 2),
     },
     {
       id: `e2`,
-      title: `Car Insurance`,
-      amount: 296.74,
-      date: new Date(2022, 4, 5),
+      title: `Anime`,
+      amount: 5000,
+      date: new Date(2022, 6, 1),
     },
     {
       id: `e3`,
-      title: `Car Insurance`,
-      amount: 296.74,
-      date: new Date(2022, 4, 5),
+      title: `Games`,
+      amount: 700,
+      date: new Date(2022, 5, 5),
     },
     {
       id: `e4`,
       title: `Car Insurance`,
-      amount: 296.74,
-      date: new Date(2022, 4, 5),
+      amount: 10000,
+      date: new Date(2022, 9, 8),
     },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>hello hello</p>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
