979f47fc1210c47b930274886fa59cc23ca9aaf1
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-08 16:44:06 +0530

    understanding button clicks

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,6 +3,10 @@ import Card from "../Ui/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const log = () => {
+    console.log(`click clicked lick`)
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +14,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={log} >Change  title</button>
     </Card>
   );
 };
