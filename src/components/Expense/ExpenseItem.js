5bae08bc113fabef1f27ff61f70178fd8cecfa6e
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-08 16:51:57 +0530

    trying to update button click action

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -3,15 +3,17 @@ import Card from "../Ui/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  let title = props.title
   const log = () => {
-    console.log(`click clicked lick`)
+    console.log(`clicked`)
+    title = "boom updated"
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={log} >Change  title</button>
