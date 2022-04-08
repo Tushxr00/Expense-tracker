15051f160232d9b8e74ecf730a9751b663109f4b
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-08 20:03:00 +0530

    tried to replicate v-model in react

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  useState();
+    let log = ""
+    const [events,setEvent] = useState(log)
+
+    const titleChangeHandler = (event) => {
+        setEvent(event.target.value)
+        console.log(event.target.value)
+        console.log("title changed")
+    }
+
   return (
     <form>
+      <p>{events}</p>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
