c3de1104c8f7eab850ee87b53feede4d43f2a3c6
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-10 20:35:47 +0530

    removed practice button

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -4,22 +4,14 @@ import Card from "../Ui/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title , setTitle] = useState(props.title)
-  
-  const log = () => {
-    setTitle(`updated `)
-    console.log(`clicked`)
-    console.log(title)
-  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={log} >Change  title</button>
     </Card>
   );
 };
