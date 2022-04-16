08897b47597a37ee5f37d958c31ea7b6cf5c19e5
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-16 11:45:57 +0530

    better and cleaner alternative

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -13,16 +13,33 @@ const Expense = (props) => {
   const filteredExpense = props.expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
+  let expenseContent = <p> no expense item found</p>
+
+  if(filteredExpense.length > 0){
+    expenseContent = filteredExpense.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
+  if (filteredExpense)
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      {expenseContent}
+
       {/* this works just like a terniary operator
       -> checks the first condition if that is true the and condition is rendered on the dom
       */}
-      {filteredExpense.length === 0 && <p> no expense occured </p>}
+      {/* {filteredExpense.length === 0 && <p> no expense occured </p>}
       {filteredExpense.length > 0 &&
         filteredExpense.map((expense) => (
           <ExpenseItem
@@ -31,7 +48,7 @@ const Expense = (props) => {
             amount={expense.amount}
             date={expense.date}
           />
-        ))}
+        ))} */}
       {/* Applying terniary operator for conditional rendering  */}
       {/* {filteredExpense.length === 0 ? (
         <p> No expenses found </p>
