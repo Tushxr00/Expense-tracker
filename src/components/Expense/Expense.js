9250b1f2dbfbd8430d895f2a494de8569a997140
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-16 11:46:31 +0530

    removed commente code

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -14,9 +14,9 @@ const Expense = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expenseContent = <p> no expense item found</p>
+  let expenseContent = <p> no expense item found</p>;
 
-  if(filteredExpense.length > 0){
+  if (filteredExpense.length > 0) {
     expenseContent = filteredExpense.map((expense) => (
       <ExpenseItem
         key={expense.id}
@@ -24,46 +24,19 @@ const Expense = (props) => {
         amount={expense.amount}
         date={expense.date}
       />
-    ))
+    ));
   }
 
   if (filteredExpense)
-  return (
-    <Card className="expenses">
-      <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
-      />
-      {expenseContent}
-
-      {/* this works just like a terniary operator
-      -> checks the first condition if that is true the and condition is rendered on the dom
-      */}
-      {/* {filteredExpense.length === 0 && <p> no expense occured </p>}
-      {filteredExpense.length > 0 &&
-        filteredExpense.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))} */}
-      {/* Applying terniary operator for conditional rendering  */}
-      {/* {filteredExpense.length === 0 ? (
-        <p> No expenses found </p>
-      ) : (
-        filteredExpense.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      )} */}
-    </Card>
-  );
+    return (
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenseContent}
+      </Card>
+    );
 };
 
 export default Expense;
